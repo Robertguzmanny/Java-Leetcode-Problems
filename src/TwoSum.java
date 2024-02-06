2d48ca8bf427685cd80ac3680b9cdffec1931853
Robertguzmanny <101305148+Robertguzmanny@users.noreply.github.com>
2024-02-06 02:10:10 -0500

    change testing on twosum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -18,10 +18,9 @@ public class TwoSum {
     }
 
     public static void main (String[] args){
-        TwoSum obj = new TwoSum();
+        TwoSum solution = new TwoSum();
         int[] nums1 = {2,7,11,15};
         int target1 = 9;
-        int[] result = obj.twosum(nums1, target1);
-        System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(solution.twosum(nums1, target1)));
     }
 }
