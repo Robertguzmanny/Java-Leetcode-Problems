d3ac7cb5b2cd6922a3996dfb836dd680b3366df8
Robertguzmanny <101305148+Robertguzmanny@users.noreply.github.com>
2024-02-05 23:57:04 -0500

    longest substring

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int maxLength = 0;
+        int[] charIndex = new int[128];
+        Arrays.fill(charIndex, -1);
+        int left = 0;
+
+        for (int right = 0; right < n; right++) {
+            if (charIndex[s.charAt(right)] >= left) {
+                left = charIndex[s.charAt(right)] + 1;
+            }
+            charIndex[s.charAt(right)] = right;
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args){
+        Main solution = new Main();
+
+        String test1 = "abcabcbb";
+        System.out.println(solution.lengthOfLongestSubstring(test1));
+
+
+
     }
 }
